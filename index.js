9240265ca9f9e45a90a3a382aab2cc8752205fb4
Lam0509 <112493001+Lam0509@users.noreply.github.com>
2022-09-08 17:12:51 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// Bài 1
+
+// let i;
+// let tong = 0;
+// while (i != -1) {
+//     i = +prompt('Nhập 1 giá trị');
+//     if (i != -1) {
+//         tong += i
+//     };
+// }
+// alert(tong);
+
+// BÀi 2
+
+// let i = 1;
+// while (i < 100) {
+//     document.write("<hr width = " + i + "%>");
+//     i++;
+// }
